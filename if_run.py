2f09d1a7cc5a6b56b3f6d0cc7682375137d7a514
Akshay <akshay.badola.cs@gmail.com>
2020-03-11 00:56:04 +0530

    Modified if_run.py for the case some other script accesses sys.argv down the line

--- if_run.py
+++ if_run.py
@@ -1,15 +1,23 @@
 import os
+import argparse
 import sys
 from trainer.interfaces import FlaskInterface
 
 
 if __name__ == '__main__':
-    hostname = sys.argv[1]
-    port = int(sys.argv[2])
-    data_dir = sys.argv[3]
-    if sys.argv[3].lower() == "false":
-        production = False
-    else:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("hostname", type=str)
+    parser.add_argument("port", type=int)
+    parser.add_argument("data_dir", type=str)
+    parser.add_argument("--production", type=str, default="False")
+    args, _ = parser.parse_known_args()
+    hostname = args.hostname
+    port = args.port
+    data_dir = args.data_dir
+    if args.production.lower() == "true":
         production = True
+    else:
+        production = False
     sys.path.append(os.path.join(data_dir, "..", ".."))
+    sys.argv = [sys.argv[0]]
     FlaskInterface(hostname, port, data_dir, production=production)

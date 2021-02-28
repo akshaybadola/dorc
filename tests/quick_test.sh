75e34b080bc524cbd3601192268c0333bc004058
Akshay <akshay.badola.cs@gmail.com>
2021-02-28 17:34:51 +0530

    Another shot at fixing the requirements. Added sphinx

--- tests/quick_test.sh
+++ tests/quick_test.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-python -m pytest -m 'quick and not todo' -k 'not test_trainer_models and not test_trainer_device'
+python -m pytest -m 'quick and not spec and not todo' -k 'not test_trainer_models and not test_trainer_device'
